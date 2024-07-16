522c4ffb38bd3191cb03e8cac9f1719fee3fb016
VoHungPhong <97014556+VoHungPhong@users.noreply.github.com>
2024-07-16 08:46:51 +0700

    Add files via upload

---
+++ server/utils/MyConstants.js
@@ -0,0 +1,11 @@
+const MyConstants = {
+    DB_SERVER: 'cluster0hp.ibojzb7.mongodb.net',
+    DB_USER: 'vhp',
+    DB_PASS: '2810',
+    DB_DATABASE: 'shoppingonline',
+    JWT_SECRET: '28102003',
+    JWT_EXPIRES: '36000000', // in milliseconds
+    EMAIL_USER: 'hungphongvo2810@gmail.com', // gmail service
+    EMAIL_PASS: 'xnof ubxo wxst ddzh'
+  };
+  module.exports = MyConstants;
